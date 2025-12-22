4c7a90822b43ccd2977a64113ddcb8421db87e72
rdbo <rdbodev@gmail.com>
2025-12-22 09:13:23 +0000

    make navbar cleaner

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,10 @@
 import { A } from "@solidjs/router";
 
 export default function NavBar() {
-  return <nav class="flex w-full bg-rose-600 mb-2 shadow-rose-200 shadow-md">
+  return <nav class="flex w-full mb-2 shadow-gray-300 shadow-md">
       <div class="bg-white flex items-center w-max pl-4 pr-8 py-2">
         <img src="/assets/wipi.svg" class="h-10 mr-2"/>
         <h1 class="text-2xl font-black text-slate-900">WiPi Web UI</h1>
-      </div>
-      <div class="w-32 bg-gradient-to-r from-white to-rose-600">
       </div>
       <div class="pr-4 w-max">
       </div>

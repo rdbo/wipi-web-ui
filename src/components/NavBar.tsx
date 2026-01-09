4d88188fc69e9fd7b8fd71599d8ca2cf01fdce74
robert. <80871202+90th@users.noreply.github.com>
2026-01-09 18:10:43 -0500

    hide nav buttons when unauthenticated (#3)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,9 +2,11 @@ import { A } from "@solidjs/router";
 import { useTheme } from "../providers/theme";
 import Button from "./ui/Button";
 import ThemeButton from "./ThemeButton";
-import { For } from "solid-js";
+import { For, Show } from "solid-js";
+import { useAuth } from "../providers/auth";
 
 export default function NavBar() {
+  const { isAuthenticated } = useAuth();
   const routes = [
     {
       "path": "/",
@@ -16,19 +18,23 @@ export default function NavBar() {
     }
   ];
   return <nav class="sticky top-0 z-50 w-full bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700 shadow-sm transition-colors duration-200">
-    <div class="container mx-auto px-4 h-16 grid grid-cols-3 items-center">
+    <div class="w-full px-8 h-16 grid grid-cols-3 items-center">
       <A href="/" class="flex items-center gap-3 h-full px-1 justify-self-start">
         <img src="/assets/wipi.svg" class="h-8 w-8" />
         <h1 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">WiPi Web UI</h1>
       </A>
-      <div class="flex items-center justify-center gap-2 justify-self-center">
-        <For each={routes}>
-          {(route) =>
-            <A href={route.path} class="w-12 h-12 rounded-full border-2 border-rose-600 flex items-center justify-center hover:bg-rose-100 dark:hover:bg-gray-700 transition-colors">
-              <img src={route.icon} class="w-8" />
-            </A>
-          }
-        </For>
+      <div class="justify-self-center">
+        <Show when={isAuthenticated()}>
+          <div class="flex items-center justify-center gap-2">
+            <For each={routes}>
+              {(route) =>
+                <A href={route.path} class="w-12 h-12 rounded-full border-2 border-rose-600 flex items-center justify-center hover:bg-rose-100 dark:hover:bg-gray-700 transition-colors">
+                  <img src={route.icon} class="w-8" />
+                </A>
+              }
+            </For>
+          </div>
+        </Show>
       </div>
       <div class="justify-self-end">
         <ThemeButton />

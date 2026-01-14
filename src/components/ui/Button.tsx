c7fa12a3ffd63007e5415269e78f2928d03c84f2
rdbo <rdbodev@gmail.com>
2026-01-14 21:36:39 +0000

    improved sigma button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,7 +4,7 @@ export default function Button({class: userClassAttr, children, ...rest}: JSX.In
   const classAttr = `
   inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 disabled:opacity-50 bg-rose-600 text-white hover:bg-rose-700 h-10 px-4 py-2 shadow-sm
   dark:focus:ring-offset-gray-900
-  sigma:bg-sigma-bright sigma:text-sigma-faint sigma:font-bold sigma:hover:text-sigma-bright sigma:hover:bg-sigma-faint sigma:focus:ring-0 sigma:focus:ring-offset-2 sigma:focus:ring-offset-sigma-bright
+  sigma:bg-sigma-bright sigma:text-sigma-faint sigma:font-bold sigma:hover:text-sigma-bright sigma:hover:bg-sigma-faint sigma:focus:ring-0 sigma:focus:ring-offset-2 sigma:focus:ring-offset-sigma-dim sigma:inset-ring-2 sigma:inset-ring-sigma-bright
   ` + (userClassAttr ? " " + userClassAttr : "");
   return <button {...rest} class={classAttr}>
     {children}
